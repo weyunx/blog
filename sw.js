9819067b3b7cc23c35a3af6746589eaefeb4c0ac
jiangnan <jn5398@163.com>
2019-01-12 17:10:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "weyunx.com",
+  "weyunx.com/blog",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
